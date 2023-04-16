994e7b12b911f13a0fc2ab8f100cc57f809d3708
LeonAngelit <leo63582@gmail.com>
2023-04-16 16:32:34 +0200

    improve performance

--- src/containers/CoursesList.jsx
+++ src/containers/CoursesList.jsx
@@ -1,27 +1,35 @@
-import React, { memo } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import useGetCourses from '@hooks/useGetCourses';
 import CourseCard from '@components/CourseCard';
 import styles from '@styles/CourseList.module.scss';
 const API = process.env.NEXT_PUBLIC_API_COURSES;
 
 const CoursesList = () => {
-  const courses = useGetCourses(API);
+  const [courses, setCourses] = useState([]);
+  let coursesProv = useGetCourses(API);
+  let coursesUpdated = useGetCourses(`${API}/update`);
+  setTimeout(() => {
+    coursesProv = coursesUpdated;
+  }, 1000);
+  useEffect(() => {
+    setCourses(coursesProv);
+  }, [coursesProv]);
+
   return (
     <>
-        <h1 className={styles["title"]}>Actualmente he realizado {courses.length} cursos</h1>
-        <h2 className={styles["subtitle"]}>
-          Haz clic en las tarjetas para ver el diploma. Estos cursos los he realizado en{' '}
-          <a className={styles["subtitle_link"]} href="https://platzi.com/" target="_blank" rel="noopener noreferrer">
-            Platzi
-          </a>{' '}
-          🙂
-        </h2>
-        <section id="cards-container" className={styles["cards-container"]}>
-        {courses.map(course =>(
-          
-             <CourseCard course={course} key={courses.indexOf(course)} />
+      <h1 className={styles['title']}>Actualmente he realizado {courses.length} cursos</h1>
+      <h2 className={styles['subtitle']}>
+        Haz clic en las tarjetas para ver el diploma. Estos cursos los he realizado en{' '}
+        <a className={styles['subtitle_link']} href="https://platzi.com/" target="_blank" rel="noopener noreferrer">
+          Platzi
+        </a>{' '}
+        🙂
+      </h2>
+      <section id="cards-container" className={styles['cards-container']}>
+        {courses.map((course) => (
+          <CourseCard course={course} key={courses.indexOf(course)} />
         ))}
-        </section>
+      </section>
     </>
   );
 };
