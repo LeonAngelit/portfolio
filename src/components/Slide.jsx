7b6d2b917292fd477d28bc7047a3b7dec6104259
LeonAngelit <leo63582@gmail.com>
2025-09-10 12:00:03 +0200

    fix url

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -3,7 +3,7 @@ import styles from '@styles/Projects.module.scss';
 export default function Slide({ image, link, title, id }) {
   return (
     <div className={styles['work']} id={id}>
-      <a href={`https://${link}`}>
+      <a href={`https://${link}`} target={'_blank'} rel={'noopener noreferrer'}>
         <img src={image} alt={title} />
       </a>
     </div>
