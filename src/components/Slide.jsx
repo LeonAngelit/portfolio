00c46a68d0d397e13f1e21d203cf204cdecef067
LeonAngelit <leo63582@gmail.com>
2025-09-14 18:26:02 +0200

    fix checkstyle

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -3,7 +3,7 @@ const API = process.env.NEXT_PUBLIC_API_PROJECTS;
 
 export default function Slide({ image, link, title, id }) {
   return (
-    <div className={styles['work']} id={id} >
+    <div className={styles['work']} id={id}>
       <a href={`https://${link}`} target={'_blank'} rel={'noopener noreferrer'} style={{ width: '80%' }}>
         <img src={`${API}/images/${image}`} alt={title} />
       </a>
